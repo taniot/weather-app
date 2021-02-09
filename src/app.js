ad5ca89e8419a86880c59e5676a2ad82598ef376
taniot <gaetano.frascolla@gmail.com>
2021-02-09 14:11:51 +0100

    Init commit

---
+++ src/app.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'taniot.dev',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    image: '/img/me.jpg',
+    name: 'taniot.dev',
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    text: 'Contact me for a coffee.',
+    name: 'taniot.dev',
+  })
+})
+
+app.get('/weather', (req, res) => {
+  const { address } = req.query
+
+  if (!address) {
+    return res.send({
+      error: 'You must provide an address.',
+    })
+  }
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      })
+    }
+    forecast(latitude, longitude, (error, forecast) => {
+      if (error) {
+        return res.send({
+          error,
+        })
+      }
+      res.send({
+        location,
+        forecast,
+        address,
+      })
+    })
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404 - Help',
+    text: 'Help article not found',
+    name: 'taniot.dev',
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 - Generic',
+    text: 'Page not found',
+    name: 'taniot.dev',
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+      address
+    )}.json?access_token=pk.eyJ1IjoidGFuaW90IiwiYSI6ImNrMXc4MGk5bjEwNmgzY21tMDhuaGJwNXQifQ.ke2NRV2lsmcp7CHJEJzQnQ`
+  
+    request({ url, json: true }, (error, {body}= {}) => {
+      if (error) {
+        callback('Unable to connect to location services!', undefined)
+      } else if (body.features.length === 0) {
+        callback('Unable to find location. Try another search', undefined)
+      } else {
+        callback(undefined, {
+          latitude: body.features[0].center[1],
+          longitude: body.features[0].center[0],
+          location: body.features[0].place_name,
+        })
+      }
+    })
+  }
+
+  module.exports = geocode
